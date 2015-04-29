7d6d787d664bd4174d507143ead403beedd389a8
Dehua <andylikescodes@gmail.com>
2015-04-29 00:36:45 -0700

    correct a mistake in the mainOS.py file

--- MainOS.py
+++ MainOS.py
@@ -16,9 +16,9 @@ database = Database("test.db")
 processor = DataProcessor()
 
 # Read original tweets from database and make them having the same number of tweets, ready for process
-ObjectiveTweets = database.readDB("manual","Tweet","Score",1)
-SubjectiveTweets = (database.readDB("manual","Tweet","Score",1) + database.readDB("manual","Tweet","Score",-1))[:len(ObjectiveTweets)]
 
+SubjectiveTweets = (database.readDB("manual","Tweet","Score",1) + database.readDB("manual","Tweet","Score",-1))
+ObjectiveTweets = database.readDB("manual","Tweet","Score",0)[:len(SubjectiveTweets)]
 
 print "Subjective Tweets: ", len(SubjectiveTweets)
 print "Objective Tweets: ", len(ObjectiveTweets)
@@ -34,18 +34,18 @@ train, test = features_maker(SubjectiveTweets, ObjectiveTweets, 'Subjective', 'O
 naiveBayes(train, test)
 
 ###############################################################
-# Subjective Tweets:  238
-# Objective Tweets:  238
-# train on 380 instances, test on 96 instances
-# accuracy: 0.5
+# Subjective Tweets:  332
+# Objective Tweets:  332
+# train on 530 instances, test on 134 instances
+# accuracy: 0.611940298507
 # Most Informative Features
-#                    store = None           Object : Subjec =      1.0 : 1.0
-#                     good = True           Object : Subjec =      1.0 : 1.0
-#                     damn = None           Object : Subjec =      1.0 : 1.0
-#                  already = None           Object : Subjec =      1.0 : 1.0
-#                 compared = True           Object : Subjec =      1.0 : 1.0
-#                  testing = None           Object : Subjec =      1.0 : 1.0
-#                      360 = None           Object : Subjec =      1.0 : 1.0
-#                   kinect = True           Object : Subjec =      1.0 : 1.0
-#                      kid = None           Object : Subjec =      1.0 : 1.0
-#                     live = None           Object : Subjec =      1.0 : 1.0
+#              playstation = True           Object : Subjec =     12.3 : 1.0
+#           forzadiscovery = True           Subjec : Object =      6.3 : 1.0
+#                 employee = True           Object : Subjec =      5.7 : 1.0
+#                     best = True           Subjec : Object =      5.7 : 1.0
+#               controller = True           Object : Subjec =      5.7 : 1.0
+#                    night = True           Subjec : Object =      5.0 : 1.0
+#                     nice = True           Subjec : Object =      4.3 : 1.0
+#                 actually = True           Subjec : Object =      4.3 : 1.0
+#                     next = True           Subjec : Object =      3.8 : 1.0
+#                    right = True           Object : Subjec =      3.8 : 1.0
